be1ec01c9c96a2c8fa320dd981e1e8f90515b5e5
willnien10005914 <32408981+willnien10005914@users.noreply.github.com>
2019-05-22 22:00:03 +0800

    python

---
+++ tf.py
@@ -0,0 +1,22 @@
+def derivative(f, x, h=0.01):
+    return (f(x+h) - f(x)) / h
+
+def square(x):
+    return x*x
+
+
+def deviation(A):
+    len = 0
+    for i in A:
+
+
+
+
+def main():
+    print("derivative : %d" % derivative(square, 2))
+    print("deviation")
+
+if __name__ == '__main__':
+    main()
+
+
