2d85e214af18fb85a9da3481bcc74cb8e7d276fe
will.nien <will.nien@quantatw.com>
2019-05-27 23:40:46 +0800

    update

--- tf.py
+++ tf.py
@@ -1,6 +1,7 @@
 import math
 import statistics
 
+
 """
 台北第一期
 """
@@ -22,7 +23,6 @@ def Deviation(X):
     return math.sqrt(result)
 
 
-
 def Fibonacci_recursive(n):
     if (n < 2):
         return n
@@ -38,7 +38,6 @@ def Fibonacci(n):
         t1 = t2
         t2 = next
     return t1
-
 
 
 """
@@ -110,8 +109,111 @@ def Taylor_Expansion(a, x, n):
 """
 台北第三期
 """
+def MinMaxScaler(data):
+    ret = []
+    min = Min(data)
+    max = Max(data)
+
+    for i in data:
+        ret.append((i - min) / (max - min))
+    return ret
 
 
+def Min(data):
+    ret = data[0]
+    for i in range(1, len(data)):
+        if ret > data[i]:
+            ret = data[i]
+    return ret
+
+
+def Max(data):
+    ret = data[0]
+    for i in range(1, len(data)):
+        if ret < data[i]:
+            ret = data[i]
+    return ret
+
+
+def PassFail(grades):
+    if grades is None:
+        return False
+
+    start = 0
+    end = len(grades)
+    status = []
+
+    while (start < end):
+        if grades[start] >= 60:
+            status.append('Pass')
+        else:
+            status.append('Fail')
+        start += 1
+    return status
+
+
+def MagicSquare(data):
+    sum_r = 0
+    sum_c = 0
+    sum_d = 0
+
+    if row_sum_check(data) is True and column_sum_check(data) is True and diagonal_sum_check(data) is True:
+        for row in range(len(data)):
+            sum_r += data[0][row]
+            sum_c += data[row][0]
+            sum_d += data[row][row]
+
+        if sum_r == sum_c == sum_d:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+
+def row_sum_check(data):
+    r = [0,]*3
+    i = 0
+    for row in data:
+        for col in row:
+            r[i] += col
+        i += 1
+
+    for i in range(len(r) - 1):
+        if r[i] != r[i+1]:
+            return False
+    return True
+
+
+def column_sum_check(data):
+    c = [0,]*3
+
+    for row in range(len(data)):
+        for col in range(len(data[row])):
+            c[col] += data[row][col]
+
+    for i in range(len(c) - 1):
+        if c[i] != c[i+1]:
+            return False
+    return True
+
+
+def diagonal_sum_check(data):
+    d = [0,]*2
+
+    length = len(data) - 1
+    i = 0
+
+    for row in range(len(data)):
+        d[0] += data[row][i]
+        d[1] += data[row][length - i]
+        i += 1
+
+    for i in range(len(d) - 1):
+        if d[i] != d[i+1]:
+            return False
+    return True
 
 """
 新竹第二期
@@ -151,13 +253,27 @@ def main():
     ans_g = g(x)
     ans_g_taylor = Taylor_Expansion(a, x, n)
     err = ((ans_g - ans_g_taylor) / ans_g) * 100
-
     print("\tg(%d) = %.2f, Taylor_Expansion(%d) = %.2f, err = %.2f%%\n" % (x, ans_g, x, ans_g_taylor, err))
 
 
+    print("\n%s" % "台北第三期 :")
+    print("1. Create a function and named it MinMaxScaler :")
+    test_arr = [4, 9, 3, 10, 0, 2]
+    print("\tMinMaxScaler : %s\n" % MinMaxScaler(test_arr))
+
+    print("2. Debug PassFail function :")
+    test_grades = [10, 60, 59, 100]
+    print("\tPassFail %s : %s\n" % (test_grades, PassFail(test_grades)))
+
+    print("3. Create a function and named it MagicSquare :")
+    test_arr = [[2, 7, 6], [9, 5, 1], [4, 3, 8]]
+    print("\tMagicSquare %s : %s\n" % (test_arr, MagicSquare(test_arr)))
 
 if __name__ == '__main__':
     main()
 
 
 
+__author__ = "Will Nien"
+__email__ = "will.nien@quantatw.com"
+__version__ = "1.0.0"
